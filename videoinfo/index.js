85bde282cf1f21459dd865edb833a20b3873b4a2
Vu Minh Thang <vuminhthang.cm@gmail.com>
2018-02-04 16:40:13 +0900

    Add ffmpeg and get video metadata

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,9 +1,19 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
   console.log('App is not ready');
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    // console.log('Video duration is:', metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
+  });
 });
